29d9944bd650d788821f4318b9a8678a89d864fe
gagan0116 <gagvam0116@gmail.com>
2026-02-02 19:28:49 -0500

    added_local_cors

--- mcp_processor/app.py
+++ mcp_processor/app.py
@@ -33,7 +33,11 @@ app = FastAPI(lifespan=lifespan)
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "https://storage.googleapis.com"
+        "https://storage.googleapis.com",
+        "http://localhost:3000",
+        "http://localhost:5500",
+        "http://127.0.0.1:3000",
+        "http://127.0.0.1:5500",
     ],
     allow_credentials=True,
     allow_methods=["*"],
